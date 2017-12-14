e9dea41da8f6ba62fa58ac6e377a7938c0b4523d
johainak <34506133+johainak@users.noreply.github.com>
2017-12-14 10:19:51 +0000

    production

--- vruua/production.py
+++ vruua/production.py
@@ -96,7 +96,9 @@ WSGI_APPLICATION = 'vruua.wsgi.application'
 #     )
 # }
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
