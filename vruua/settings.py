b88cb610c27fb246cea1f4b8cc98d6b204b507b3
johainak <34506133+johainak@users.noreply.github.com>
2017-12-14 10:50:04 +0000

    db settings

--- vruua/settings.py
+++ vruua/settings.py
@@ -95,8 +95,13 @@ WSGI_APPLICATION = 'vruua.wsgi.application'
 #     )
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 DATABASES = {
-    'default': dj_database_url.config()
+  'default': dj_database_url.config(
+      default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+  )
 }
 
 # Password validation
